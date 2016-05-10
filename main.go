13d08d112917205ce2c95328046a19f876bb7db4
Sion Williams <sion@byteshifter.io>
2016-05-10 19:53:54 +0100

    chore: add dependency management using Glide

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/willis7/Alice/parser"
+)
+
+func main() {
+	s := `My Clippings.txt`
+	clips := parser.Parse(s)
+
+	fmt.Printf("clips = %#v \n", clips)
 }
